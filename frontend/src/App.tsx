9df8c6dd19e34342f91b48d1dab8beece56a7a25
akshay-it23 <it23039@glbitm.ac.in>
2025-10-11 17:02:53 +0530

    fixed frontend submodule issue

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/ui/Button'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Button variant="primary" text='share'/>
+            <Button variant="primary" text='add content'/>
+    </>
+  )
+}
+
+export default App
