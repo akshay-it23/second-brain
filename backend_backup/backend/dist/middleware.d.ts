014227885e3bde0a04bb9f8c6b49d8031c7f092b
akshay-it23 <it23039@glbitm.ac.in>
2025-10-15 11:15:19 +0530

    final project

--- backend_backup/backend/dist/middleware.d.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export interface AuthRequest extends Request {
-    userId?: string;
-}
-export declare const userMiddleware: (req: AuthRequest, res: Response, next: NextFunction) => void;
